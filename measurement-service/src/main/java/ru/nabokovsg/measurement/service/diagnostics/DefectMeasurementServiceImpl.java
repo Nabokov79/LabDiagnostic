dba5385110f2f54a42b3b15a7606456ece1f920d
Nabokov79 <nabokovsg@yandex.ru>
2025-01-19 18:50:02 +0300

    update

--- measurement-service/src/main/java/ru/nabokovsg/measurement/service/diagnostics/DefectMeasurementServiceImpl.java
+++ measurement-service/src/main/java/ru/nabokovsg/measurement/service/diagnostics/DefectMeasurementServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.nabokovsg.measurement.service.diagnostics;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.nabokovsg.measurement.client.MeasurementClient;
 import ru.nabokovsg.measurement.dto.client.EquipmentDto;
