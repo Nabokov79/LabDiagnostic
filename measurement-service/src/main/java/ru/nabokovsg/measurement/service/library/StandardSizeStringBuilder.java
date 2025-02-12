f97632ed782a6791ad36189ec0588523a34acdb1
NabokovSG <nabokovsg@yandex.ru>
2025-02-12 16:22:00 +0300

    delete

--- measurement-service/src/main/java/ru/nabokovsg/measurement/service/library/StandardSizeStringBuilder.java
+++
@@ -1,8 +0,0 @@
-package ru.nabokovsg.measurement.service.library;
-
-import ru.nabokovsg.measurement.model.library.StandardSize;
-
-public interface StandardSizeStringBuilder {
-
-    String convertToString(StandardSize standardSize);
-}
